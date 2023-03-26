d20610cd5015989bc3a71b7453994ee3cd66ca0e
KyroC <Kyroeth@gmail.com>
2023-03-26 23:26:15 +0800

    Added build

--- part3/index.js
+++ part3/index.js
@@ -7,6 +7,7 @@ const app = express()
 app.use(express.json());
 app.use(morgan('tiny'))
 app.use(cors())
+app.use(express.static('build'));
 
 
 let persons = [
@@ -86,7 +87,7 @@ app.get('/info', (request, response) => {
     `)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
-})
+}) 
